e931b342fcc6bd7813d467f02f7b962e1930b17a
talm <morganl@deakin.edu.au>
2023-03-02 15:27:58 +1100

    Package Test

---
+++ src/PatternA/PatternA.java
@@ -0,0 +1,11 @@
+package PatternA;
+
+public class PatternA 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test");
+	}
+
+}
---
+++ src/PatternB/PatternB.java
@@ -0,0 +1,12 @@
+package PatternB;
+
+public class PatternB 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test 2");
+
+	}
+
+}
