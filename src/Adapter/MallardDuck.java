b04c224b65cd860c8e49f369e0a33522e0aac9b9
lmorg42 <morganl@deakin.edu.au>
2023-07-07 13:17:56 +1000

    Added Adapter

---
+++ src/Adapter/MallardDuck.java
@@ -0,0 +1,17 @@
+package Adapter;
+
+public class MallardDuck implements Duck {
+
+	@Override
+	public void quack() 
+	{
+		System.out.println("Quack");
+	}
+
+	@Override
+	public void fly() 
+	{
+		System.out.println("Flying");
+	}
+
+}
